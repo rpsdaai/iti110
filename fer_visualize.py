52574054f4993cf0d13e362b32c8a2fcf7aa8297
rpsdaai <52661288+rpsdaai@users.noreply.github.com>
2022-02-19 22:09:29 +0800

    Add files via upload
    Added visualkeras model visualizer

--- fer_visualize.py
+++ fer_visualize.py
@@ -7,6 +7,19 @@ from sklearn.metrics import confusion_matrix
 import logging
 import sys
 
+
+# pip install visualkeras (dependencies on aggdraw)
+# pip install aggdraw 
+#
+# To install aggdraw on windows, download https://github.com/ubawurinna/freetype-windows-binaries
+# copy the files under include directory to D:\Program Files\Java\jdk-11.0.9\include and the files
+# under freetype-windows-binaries-master\release dll\win64 (freetype.lib, freetype.dll) to  D:\Program Files\Java\jdk-11.0.9\lib
+#
+import visualkeras
+from PIL import ImageFont
+from matplotlib import font_manager
+from PIL import Image, ImageDraw
+
 # Global variable
 emotion_description = {0:"Angry", 1:"Disgust", 2:"Fear", 3:"Happy", 4:"Sad", 5:"Surprise", 6:"Neutral"}
 emotion_labels = [
@@ -22,7 +35,7 @@ emotion_labels = [
 # __name__ contains the full name of the current module
 log = logging.getLogger(__name__)
 
-def plot_distribution(df):
+def plot_distribution(df, filename):
     log.info('--> plot_distribution()\n')
     # Ref: https://www.kaggle.com/lxyuan0420/facial-expression-recognition-using-cnn
     emotion_counts = df['emotion'].value_counts(sort=False).reset_index()
@@ -44,9 +57,10 @@ def plot_distribution(df):
     plt.title('Facial Emotion Frequency Distribution')
     plt.ylabel('Count', fontsize=12)
     plt.xlabel('Emotion Types', fontsize=12)
+    plt.savefig(filename, dpi=300)
     plt.show()
 
-def plot_sample_faces(df):
+def plot_sample_faces(df, filename):
     log.info('--> plot_sample_faces()\n')
     fig = plt.figure(1, (14, 14))
 
@@ -63,7 +77,7 @@ def plot_sample_faces(df):
             ax.set_yticks([])
             ax.set_title(emotion_description[label])
             plt.tight_layout()
-
+    plt.savefig(filename, dpi=300)
     plt.show()
 
 def plot_loss(history, filename):
@@ -111,7 +125,26 @@ def plot_confusion_matrix(y_true, y_pred):
                      annot_kws={"size": 20})
     ax.set(xlabel='Predicted label', ylabel='True label')
 
+def plot_dl_architecture(model, filename):
+    log.info('-->plot_dl_architecture(): filename = ' + filename)
+
+    # Install true type fonts
+    # Ref: https://www.technipages.com/windows-10-how-to-install-truetype-fonts
+    font = font_manager.FontProperties(family='sans-serif', weight='bold')
+    file = font_manager.findfont(font)
+    print(file)
+
+    # Ref: https://stackoverflow.com/questions/66274858/choosing-a-pil-imagefont-by-font-name-rather-than-filename-and-cross-platform-f
+    # Ref: https://stackoverflow.com/questions/61518366/is-possible-to-link-an-online-font-file-to-pil-imagefont-truetype
+    font = ImageFont.truetype("D:\\ProgramData\\Anaconda3\\lib\\site-packages\\matplotlib\\mpl-data\\fonts\\ttf\\DejaVuSans-Bold.ttf", 42, encoding="unic")
+    # Ref: https://openprojectrepo.com/project/paulgavrikov-visualkeras-python-data-validation
+    visualkeras.layered_view(model, legend=True, font=font, to_file=filename).show()
+
 # plot_distribution(df_train)
 # plot_sample_faces(df_train)
 # plot_loss(history, 'loss')
 # plot_accuracy(history, 'accuracy')
+if __name__ == '__main__':
+    font = font_manager.FontProperties(family='sans-serif', weight='bold')
+    file = font_manager.findfont(font)
+    print(file)
