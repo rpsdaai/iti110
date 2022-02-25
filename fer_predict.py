1f7214f42931522420557a3a19a349d2f0321273
rpsdaai <52661288+rpsdaai@users.noreply.github.com>
2022-02-25 21:45:59 +0800

    Added predict emotion from video and stills

--- fer_predict.py
+++ fer_predict.py
@@ -10,6 +10,72 @@ from tensorflow.keras.preprocessing.image import img_to_array
 # __name__ contains the full name of the current module
 log = logging.getLogger(__name__)
 
+def predict_emotion(json_file, weights_file, imageFile):
+    # Load model from JSON file
+    # with open(json_file, 'r') as j_file:
+    # loaded_json_model = j_file.read()
+    # j_file.close()
+    
+    loaded_model = model_from_json(open(json_file, "r").read()) 
+
+    # Load weights and them to model
+    loaded_model.load_weights(weights_file)
+
+    face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    img = cv2.imread(imageFile)
+    
+    # Convert RGB image to grayscale
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Detects objects of different sizes in the input image. The detected objects 
+    # are returned as a list of rectangles. Objects smaller than that are ignored.
+    faces_detected = face_detector.detectMultiScale(gray_img, 1.18, 5)
+      
+    for (x, y, w, h) in faces_detected:
+        # cv2.rectangle(image, start_point, end_point, color, thickness); draw rectangle
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        roi_gray = gray_img[y:y + w, x:x + h]
+        roi_gray = cv2.resize(roi_gray, (48, 48))
+        
+        img_pixels = image.img_to_array(roi_gray)
+        img_pixels = np.expand_dims(img_pixels, axis=0)
+        img_pixels /= 255.0
+
+        predictions = loaded_model.predict(img_pixels)
+        
+        # returns the position of the largest value
+        max_index = int(np.argmax(predictions))
+#         log.info ('max_index: ', str(max_index))
+        
+        # emotions = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear']
+        emotions = [
+            'Angry',
+            'Disgust',
+            'Fear',
+            'Happy',
+            'Sad',
+            'Surprise',
+            'Neutral'
+        ]
+        predicted_emotion = emotions[max_index]
+
+        # image on which you can write the text.
+        # text you want to write on image.
+        # position: distance along horizontal and vertical axis from top left corner of the image.
+        # font family
+        # font size
+        # font color
+        # font stroke width
+        cv2.putText(img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
+
+    resized_img = cv2.resize(img, (1024, 768))
+    cv2.imshow('Facial Emotion Recognition', resized_img)
+
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+
 def predict_emotion_from_saved_model_file(saved_model_file, imageFile):
     log.info('predict_emotion(): model_file = ' + saved_model_file + ' imaggeFile = ' + imageFile)
 
@@ -136,12 +202,13 @@ if __name__ == '__main__':
     # predict_emotion_from_saved_model_file('models\\dense09012022', 'D:\\Users\\ng_a\\My NYP SDAAI\\PDC-2\\ITI110\\test_images\\sad.jpg') 
     # predict_emotion_from_saved_model_file('models\\dense09012022', 'D:\\Users\\ng_a\\My NYP SDAAI\\PDC-2\\ITI110\\test_images\\surprise.jpg')
 
+    now = '06022022-093427'
     # json_file = 'densenet121-' + now + '.json'
     # weights_file = 'densenet121-' + now + '.h5'
-    # model_file = os.curdir + '/models/' + 'densenet121-' + now
+    # model_file = os.curdir + '/models/' + 'densenet121-06022022' + now
     # log.info('json_file: ' + json_file + ' weights_file: ' + weights_file)
     # for (root,dirs,files) in os.walk('test_images', topdown=True):
     #     for file in files:
-    #         # predict_emotion('models/'+json_file, 'models/'+weights_file, 'test_images/' + file)
-    #         predict_emotion_from_saved_model_file(model_file, 'test_images/' + file)
-    predict_emotion_from_video('models\\dense09012022')
+    #         predict_emotion('models/densenet121-06022022/'+json_file, 'models/densenet121-06022022/'+weights_file, 'test_images/' + file)
+    #         # predict_emotion_from_saved_model_file(model_file, 'test_images/' + file)
+    predict_emotion_from_video('models\\densenet121-06022022\\densenet121-06022022-093427\\')
